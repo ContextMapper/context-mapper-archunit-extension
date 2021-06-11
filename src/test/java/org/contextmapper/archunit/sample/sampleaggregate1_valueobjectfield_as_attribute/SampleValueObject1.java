f6a345834323e0e8edba00e26a0e8d0a5208dd3d
Stefan Kapferer <stefan.kapferer@mimacom.com>
2021-06-11 07:57:39 +0200

    Bugfix: ignore static fields on Java classes

--- src/test/java/org/contextmapper/archunit/sample/sampleaggregate1_valueobjectfield_as_attribute/SampleValueObject1.java
+++ src/test/java/org/contextmapper/archunit/sample/sampleaggregate1_valueobjectfield_as_attribute/SampleValueObject1.java
@@ -5,6 +5,8 @@ import org.jmolecules.ddd.annotation.ValueObject;
 @ValueObject
 public class SampleValueObject1 {
 
+    private static final String STATIC_FIELDS_MUST_BE_IGNORED = "";
+
     private String sampleField;
 
 }
